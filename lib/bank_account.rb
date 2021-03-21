b62a49d9d55cc97c97b2b12de9633eda897db0c4
sumeikom <sumeikom@gmail.com>
2021-03-21 08:05:17 -0400

    commit tree a58b18dff4d0d0fbb16c47ffef66a21fd7f28ade

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,30 @@
 class BankAccount
-
-end
+    attr_reader :name, :deposit
+    attr_accessor :balance, :status
+  
+    def initialize(name, balance=1000, status="open")
+      @name = name
+      @balance = balance
+      @status = status
+    end
+  
+    def deposit(deposit)
+      @balance += deposit
+    end
+  
+    def display_balance
+      "Your balance is $#{@balance}."
+    end
+  
+    def valid?
+      if @balance > 0 && @status == "open"
+        true
+      else
+        false
+      end
+    end
+  
+    def close_account
+      @status = "closed"
+    end
+  end
